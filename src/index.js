f4e0af5f6188ded3b1160a743f06c609829bde24
PC-Rat <32428513+PC-Rat@users.noreply.github.com>
2021-08-06 14:53:43 +0300

    feat: add code for ones

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 module.exports = function toReadable (number) {
-    var converter = require('number-to-words');
-	converter.toWords(n);
-	converter.toWordsOrdinal(n);
-  
+    //var converter = require('number-to-words');
+	//converter.toWords(n);
+	//converter.toWordsOrdinal(n);
+	const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+	/*const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+	const decs = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];*/
+
+	const readOnes = (ones, i) => {
+		for (i = 0; i++; i < ones.length) {
+			return console.log(ones[i]);
+		}
+	};
+	return s;
 }
