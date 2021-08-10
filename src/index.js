8ed52901d62665fa292e37fccbf6b9b7345d4583
PC-Rat <32428513+PC-Rat@users.noreply.github.com>
2021-08-10 12:07:50 +0300

    refactor: function

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-module.exports = function toReadable (number) {}
-    //var converter = require('number-to-words');
+module.exports = function toReadable(number) {
+	//fail to do it myself, so:
+	//npm install number-to-words
+	//and!
+	//var converter = require('number-to-words');
 	//converter.toWords(n);
 	//converter.toWordsOrdinal(n);
-	function toReadable(s) {
+	//it's really cool
+
 	const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 	const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 	/*const decs = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];*/
 
 	const getOnes = (ones, i) => {
 		for (i = 0; i++; i < ones.length) {
-			console.log(getOnes());
-		}
-	};
-	const getOnes = (ones, i) => {
-		for (i = 0; i++; i < ones.length) {
-			console.log(getOnes());
+			return ones;
 		}
 	};
 	const getTeens = (teens, i) => {
 		for (i = 0; i++; i < teens.length) {
-			console.log(getTeens());
+			return teens;
 		}
 	};
 }
-
